5a50628448e88cae5be9ff7b941305b4cfbdfdcd
Kragh <eaa23chkr@students.eaaa.dk>
2024-02-08 11:10:34 +0100

    Christian commit

--- src/Opgave1/App.java
+++ src/Opgave1/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Halløj");
         System.out.println("Jeg skal se True Detective i weekenden O_O ");
+        System.out.println("Booster Twisted Ice == win");
     }
 }
